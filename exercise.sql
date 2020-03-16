a7dcb7761c34d100073e50f989b96bd8c8757c23
HENRY BENAYE <2106747@talnet.nl>
2020-03-16 13:09:46 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,15 +1,5 @@
-
-alter table planeten
-add column id int NOT NULL auto_increment primary key;
-insert into planeten (naam, diameter, afstand_tot_de_zon, massa) values("Mars", 6794, 227936640, 0.1)
-
-
-
-
-
-
-
-
-
-
-
+update planeten
+set 
+	naam = 'Teenalp'
+where
+	id = 6;
